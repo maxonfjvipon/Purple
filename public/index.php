a72f4466eecb7f096c2c606dd4f28b17f464ba04
maximtrunnikov <m.trunnikov@mail.ru>
2022-10-03 20:17:14 +0300

    remove delete

--- public/index.php
+++ public/index.php
@@ -22,7 +22,6 @@ try {
                 new RtGet(new RtUri("edit", new EpEdit())),
                 new RtPost(new RtUri("/", new EpCreate())),
                 new RtPut(new RtUri('update', new EpUpdate())),
-                new RtDelete(new RtUri('/', new EpDelete())),
             ))
         ))
     ))->process();
